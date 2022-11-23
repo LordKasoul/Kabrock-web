6e3524fdeed23023c439fe8172288775c7e7d8a4
LordKasoul <107035954+LordKasoul@users.noreply.github.com>
2022-11-23 01:38:44 +0100

    Initial commit
    C'est le code de base ( qui marche ) su site. Tout est pas au point mais ou moins c'est propre pour un début.

---
+++ cours.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>   
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link ref="shortcut icon" href="icon kabrock 16.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <title>Kabrock web</title>
+    </head>
+    <body>
+        <header>        
+            <a href="index.php">
+                <img class="logo" src="kabrock-removebg-preview.png">
+            </a>
+            <div class="header_div">
+                <h1>Kabrock Web</h1>
+                </br>
+                <h2>Index</h2>
+            </div>
+            <div class="topright">
+                <h3>Connection<h3>
+            </div>
+        </header>
+        <div class="media">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/8tAJ-EAPu0U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+    <head>   
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link ref="shortcut icon" href="icon kabrock 16.png">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet">
+    <title>Kabrock web</title>
+    </head>
+    <body>
+        <header>        
+            <a href="index.php">
+                <img class="logo" src="/image/kabrock-removebg-preview.png">
+            </a>
+            <div class="header_div">
+                <h1>Kabrock Web</h1>
+                </br>
+                <h2>Index</h2>
+            </div>
+            <div class="topright">
+                <h3>Connection<h3>
+            </div>
+        </header>
+
+        <div class="options">
+            <div class="librairie">
+                <p> · Bibliotheque</p>
+            </div>
+            <div class="bot">
+                <p> · Bots</p>
+            </div>
+            <div class="web">
+                <p> · Sites web</p>
+            <div  class="bts">
+                <a  href="cours.php">
+                <p> · Cours</p></a>
+            </div>
+        </div>
+    </body>
+</html>
