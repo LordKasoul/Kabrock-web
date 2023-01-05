74e9bb58ab442765503bc73933ac8433e2e7e33e
LordKasoul <107035954+LordKasoul@users.noreply.github.com>
2023-01-05 21:00:13 +0100

    test to fix the connection to account 3

--- connection.php
+++ connection.php
@@ -43,19 +43,6 @@
             <a href="sing-in.php"><h4>S'inscrire</h4></a>$_COOKIE-->
             
             <?php
-                define('DB_SERVER', 'localhost');
-                define('DB_USERNAME', 'root');
-                define('DB_PASSWORD', '');
-                define('DB_NAME', 'registration');
-                 
-                // Connexion à la base de données MySQL 
-                $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-                 
-                // Vérifier la connexion
-                if($conn === false){
-                    die("ERREUR : Impossible de se connecter. " . mysqli_connect_error());
-                }
-
                 session_start();
 
                 if (isset($_POST['username'])){
