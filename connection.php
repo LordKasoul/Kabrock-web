7cc75705e887aaa4384d6c5d9a3ae1856e24b101
LordKasoul <107035954+LordKasoul@users.noreply.github.com>
2023-01-07 01:53:30 +0100

    test to fix the connection 9

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html>
-    <head>   
+    <head>
     <meta charset="utf-8">
     <link rel="shortcut icon" href="#">
     <link rel="stylesheet" type="text/css" href="style.css">
@@ -11,16 +11,16 @@
     <title>Kabrock web</title>
     </head>
     <body>
-        <header>        
+        <header>
             <a href="index.php">
                 <img class="logo" src="/image/Kabrock-logo-remove test 2.png">
             </a>
             <div class="header_div">
                 <h1>Kabrock Web</h1>
                 </br>
-                <div class="breadcrumbs"> <!--Chemin d'accès--> 
+                <div class="breadcrumbs"> <!--Chemin d'accès-->
                     <h2><a href="index.php">Index</a></h2>
-                    <p>&nbsp;&gt; </p> <!--Permet de faire un espace + '>' et de finir avec un espace--> 
+                    <p>&nbsp;&gt; </p> <!--Permet de faire un espace + '>' et de finir avec un espace-->
                     <h2><a href="connection.php">Connection</a></h2>
                 </div>
             </div>
@@ -31,7 +31,6 @@
         <div class="ZoneSaisie">
 
             <!-- Ma partie a moi -->
-
             <!--    <h3>Connection</h3>
                 <form action="connection.php" method="POST" class="connect">
                     <label for="fname">Nom d'utilisateur :</label><br>
@@ -52,18 +51,17 @@
                 $_SESSION['username'] = $username;
                 $password = stripslashes($_REQUEST['password']);
                 $password = mysqli_real_escape_string($conn, $password);
-                    $query = "SELECT * FROM `users` WHERE username='$username' 
-                and password='".hash('sha256', $password)."'";
-                
+                $query = "SELECT * FROM `users` WHERE username='$username'and password='".hash('sha256', $password)."'";
+
                 $result = mysqli_query($conn,$query) or die(mysqli_error($conn));
                 
                 if (mysqli_num_rows($result) == 1) {
                     $user = mysqli_fetch_assoc($result);
                     // vérifier si l'utilisateur est un administrateur ou un utilisateur
                     if ($user['type'] == 'admin') {
-                    header('location: admin/home.php');      
+                    header('location: admin/home.php'); 
                     }else{
-                    header('location: index.php');
+                    header('location: accounts.php');
                     }
                 }else{
                     $message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
